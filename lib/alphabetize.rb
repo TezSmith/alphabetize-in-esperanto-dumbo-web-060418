13ea8b62b3e7a3f519c54c082fc9aa9e5b8bae53
TezSmith <Montez.Smith1@gmail.com>
2018-05-28 14:00:45 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,18 @@
+
+require "pry"
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+#This method will
+# alphabetizes based on the first letter
+# alphabetizes based on second letters
+# alphabetizes based on sixth letters
+# alphabetizes based on second letters not in English alphabet
+
 def alphabetize(arr)
-  # code here
+  #sorts strings in an array alphabetically
+  arr.sort_by do |string|
+    string.chars.map do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+    end
+  end
 end
